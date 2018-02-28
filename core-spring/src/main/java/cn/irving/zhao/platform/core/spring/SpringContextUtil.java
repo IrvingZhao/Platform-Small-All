7e080546ba9702c1672edab879540ea3ff7966b9
zhao jianan <zhaojn1@lenovo.com>
2018-02-28 17:34:17 +0800

    添加 spring context 方法

--- core-spring/src/main/java/cn/irving/zhao/platform/core/spring/SpringContextUtil.java
+++ core-spring/src/main/java/cn/irving/zhao/platform/core/spring/SpringContextUtil.java
@@ -2,12 +2,22 @@ package cn.irving.zhao.platform.core.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.*;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.lang.Nullable;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * 让静态变量保存到Spring ApplicationContext
@@ -24,7 +34,6 @@ public class SpringContextUtil implements ApplicationContextAware, DisposableBea
      * 取得存储在静态变量中的ApplicationContext.
      */
     public static ApplicationContext getApplicationContext() {
-        assertContextInjected();
         return applicationContext;
     }
 
@@ -60,52 +69,177 @@ public class SpringContextUtil implements ApplicationContextAware, DisposableBea
     }
 
     /**
-     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T getBean(String name) {
-        assertContextInjected();
-        return (T) applicationContext.getBean(name);
-    }
-
-    /**
-     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
-     */
-    public static <T> T getBean(Class<T> requiredType) {
-        assertContextInjected();
-        return applicationContext.getBean(requiredType);
-    }
-
-    public static <T> T getBean(String name, Class<T> requiredType) {
-        assertContextInjected();
-        return applicationContext.getBean(name, requiredType);
-    }
-
-    /**
-     * 清除SpringContextUtil中的ApplicationContext为Null.
-     */
-    public static void clearHolder() {
-        if (logger.isDebugEnabled()) {
-            logger.debug("清除SpringContextUtil中的ApplicationContext:" + applicationContext);
-        }
-        applicationContext = null;
-    }
-
-    /**
-     * 检查ApplicationContext不为空.
-     */
-    private static void assertContextInjected() {
-        if(applicationContext!=null){
-            throw new IllegalStateException("applicaitonContext属性未注入, 请在applicationContext.xml中定义SpringContextUtil.");
-        }
-    }
-
-    /**
      * 实现DisposableBean接口, 在Context关闭时清理静态变量.
      */
     @Override
     public void destroy() throws Exception {
-        SpringContextUtil.clearHolder();
+        applicationContext = null;
     }
 
+    @Nullable
+    public static String getId() {
+        return applicationContext.getId();
+    }
+
+    public static String getApplicationName() {
+        return applicationContext.getApplicationName();
+    }
+
+    public static String getDisplayName() {
+        return applicationContext.getDisplayName();
+    }
+
+    public static long getStartupDate() {
+        return applicationContext.getStartupDate();
+    }
+
+    @Nullable
+    public static ApplicationContext getParent() {
+        return applicationContext.getParent();
+    }
+
+    public static AutowireCapableBeanFactory getAutowireCapableBeanFactory() throws IllegalStateException {
+        return applicationContext.getAutowireCapableBeanFactory();
+    }
+
+    public static Environment getEnvironment() {
+        return applicationContext.getEnvironment();
+    }
+
+    public static boolean containsBeanDefinition(String beanName) {
+        return applicationContext.containsBeanDefinition(beanName);
+    }
+
+    public static int getBeanDefinitionCount() {
+        return applicationContext.getBeanDefinitionCount();
+    }
+
+    public static String[] getBeanDefinitionNames() {
+        return applicationContext.getBeanDefinitionNames();
+    }
+
+    public static String[] getBeanNamesForType(ResolvableType type) {
+        return applicationContext.getBeanNamesForType(type);
+    }
+
+    public static String[] getBeanNamesForType(Class<?> type) {
+        return applicationContext.getBeanNamesForType(type);
+    }
+
+    public static String[] getBeanNamesForType(Class<?> type, boolean includeNonSingletons, boolean allowEagerInit) {
+        return applicationContext.getBeanNamesForType(type, includeNonSingletons, allowEagerInit);
+    }
+
+    public static <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
+        return applicationContext.getBeansOfType(type);
+    }
+
+    public static <T> Map<String, T> getBeansOfType(Class<T> type, boolean includeNonSingletons, boolean allowEagerInit) throws BeansException {
+        return applicationContext.getBeansOfType(type, includeNonSingletons, allowEagerInit);
+    }
+
+    public static String[] getBeanNamesForAnnotation(Class<? extends Annotation> annotationType) {
+        return applicationContext.getBeanNamesForAnnotation(annotationType);
+    }
+
+    public static Map<String, Object> getBeansWithAnnotation(Class<? extends Annotation> annotationType) throws BeansException {
+        return applicationContext.getBeansWithAnnotation(annotationType);
+    }
+
+    @Nullable
+    public static <A extends Annotation> A findAnnotationOnBean(String beanName, Class<A> annotationType) throws NoSuchBeanDefinitionException {
+        return applicationContext.findAnnotationOnBean(beanName, annotationType);
+    }
+
+    public static Object getBean(String name) throws BeansException {
+        return applicationContext.getBean(name);
+    }
+
+    public static <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+        return applicationContext.getBean(name, requiredType);
+    }
+
+    public static Object getBean(String name, Object... args) throws BeansException {
+        return applicationContext.getBean(name, args);
+    }
+
+    public static <T> T getBean(Class<T> requiredType) throws BeansException {
+        return applicationContext.getBean(requiredType);
+    }
+
+    public static <T> T getBean(Class<T> requiredType, Object... args) throws BeansException {
+        return applicationContext.getBean(requiredType, args);
+    }
+
+    public static boolean containsBean(String name) {
+        return applicationContext.containsBean(name);
+    }
+
+    public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+        return applicationContext.isSingleton(name);
+    }
+
+    public static boolean isPrototype(String name) throws NoSuchBeanDefinitionException {
+        return applicationContext.isPrototype(name);
+    }
+
+    public static boolean isTypeMatch(String name, ResolvableType typeToMatch) throws NoSuchBeanDefinitionException {
+        return applicationContext.isTypeMatch(name, typeToMatch);
+    }
+
+    public static boolean isTypeMatch(String name, Class<?> typeToMatch) throws NoSuchBeanDefinitionException {
+        return applicationContext.isTypeMatch(name, typeToMatch);
+    }
+
+    @Nullable
+    public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
+        return applicationContext.getType(name);
+    }
+
+    public static String[] getAliases(String name) {
+        return applicationContext.getAliases(name);
+    }
+
+    @Nullable
+    public static BeanFactory getParentBeanFactory() {
+        return applicationContext.getParentBeanFactory();
+    }
+
+    public static boolean containsLocalBean(String name) {
+        return applicationContext.containsLocalBean(name);
+    }
+
+    @Nullable
+    public static String getMessage(String code, Object[] args, String defaultMessage, Locale locale) {
+        return applicationContext.getMessage(code, args, defaultMessage, locale);
+    }
+
+    public static String getMessage(String code, Object[] args, Locale locale) throws NoSuchMessageException {
+        return applicationContext.getMessage(code, args, locale);
+    }
+
+    public static String getMessage(MessageSourceResolvable resolvable, Locale locale) throws NoSuchMessageException {
+        return applicationContext.getMessage(resolvable, locale);
+    }
+
+    public static void publishEvent(ApplicationEvent event) {
+        applicationContext.publishEvent(event);
+    }
+
+    public static void publishEvent(Object event) {
+        applicationContext.publishEvent(event);
+    }
+
+    public static Resource[] getResources(String s) throws IOException {
+        return applicationContext.getResources(s);
+    }
+
+    public static Resource getResource(String s) {
+        return applicationContext.getResource(s);
+    }
+
+    @Nullable
+    public static ClassLoader getClassLoader() {
+        return applicationContext.getClassLoader();
+    }
 }
